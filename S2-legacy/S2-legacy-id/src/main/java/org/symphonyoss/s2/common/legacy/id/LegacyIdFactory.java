1c272f88d27f31df195785d1a93db73ef2a1cb8b
byron <byron@symphony.com>
2018-04-10 17:25:32 -0700

    change the composition of maestro message  - remove fromPod

--- S2-legacy/S2-legacy-id/src/main/java/org/symphonyoss/s2/common/legacy/id/LegacyIdFactory.java
+++ S2-legacy/S2-legacy-id/src/main/java/org/symphonyoss/s2/common/legacy/id/LegacyIdFactory.java
@@ -31,21 +31,21 @@ import org.symphonyoss.s2.common.hash.HashProvider;
 /**
  * A factory to create inferred Hash values each of which is
  * a 2.0 ID which acts as a mirror for an object in the legacy (Symphony1.5) space.
- * 
+ *
  * Many methods take a tenantId which it uses to generate a globally unique ID for each
  * object across all pods. This means that if the same threadId happens to exist in 2
  * separate pods and we create a 2.0 Sequence to represent those two threads that the
  * 2.0 IDs will not collide in a multi tenant environment.
- * 
+ *
  * This also means that where the same message goes xpod that events about the same message
  * originating from different pods will have different event IDs. In the comments below
  * where it says "originating pod" it means the pod where the event originates which may
  * be different from the pod where the message originated.
- * 
+ *
  * Note that where we refer to userId in this class that we expect the "external" userId
- * which already includes the tenantId and is already globally unique. In this case the 
+ * which already includes the tenantId and is already globally unique. In this case the
  * tenantId is not included again.
- * 
+ *
  * @author Bruce Skingle
  *
  */
@@ -53,7 +53,7 @@ public class LegacyIdFactory
 {
   /**
    * Create a 2.0 Hash (ID) for the given messageId.
-   * 
+   *
    * @param userId      An external userId
    * @return            The 2.0 object ID for the mirror of the given ID.
    */
@@ -61,10 +61,10 @@ public class LegacyIdFactory
   {
     return HashProvider.getCompositeHashOf(LegacyId.USER_ID, userId);
   }
-  
+
   /**
    * Create a 2.0 Hash (ID) for the given messageId.
-   * 
+   *
    * @param tenantId    The tenant ID of the pod where this event originated.
    * @param messageId   A messageId
    * @return            The 2.0 object ID for the mirror of the given ID.
@@ -74,10 +74,10 @@ public class LegacyIdFactory
   {
     return HashProvider.getCompositeHashOf(LegacyId.MESSAGE_ID, tenantId, messageId);
   }
-  
+
   /**
    * Create a 2.0 Hash (ID) for the given threadId.
-   * 
+   *
    * @param tenantId    The tenant ID of the pod where this event originated.
    * @param threadId    A threadId
    * @return            The 2.0 object ID for the mirror of the given ID.
@@ -90,7 +90,7 @@ public class LegacyIdFactory
 
   /**
    * Create a 2.0 Hash (ID) for the read receipt of the given messageId for the given userId.
-   * 
+   *
    * @param tenantId    The tenant ID of the pod where this event originated.
    * @param userId      The external (globally unique) user ID or the user who read the message.
    * @param messageId   The id of the message read.
@@ -105,7 +105,7 @@ public class LegacyIdFactory
 
   /**
    * Create a 2.0 Hash (ID) for the delivery receipt of the given messageId for the given userId.
-   * 
+   *
    * @param tenantId    The tenant ID of the pod where this event originated.
    * @param userId      The external (globally unique) user ID or the user who read the message.
    * @param messageId   The id of the message read.
@@ -120,29 +120,27 @@ public class LegacyIdFactory
 
   /**
    * Create a 2.0 Hash (ID) for a MaestroMessage.
-   * 
+   *
    * @param tenantId    The tenant ID of the pod where this event originated.
    * @param typeName          The type of the MaestroPayload.
-   * @param fromPod           The originating pod id.
    * @param messageId         The message ID.
    * @param threadId          The thread ID.
    * @return            The 2.0 object ID for the mirror of the given ID.
    * @throws NullPointerException if any parameter is null.
    */
-  public Hash maestroId(String tenantId, String typeName, Integer fromPod, byte[] messageId, @Nullable byte[] threadId)
+  public Hash maestroId(String tenantId, String typeName, byte[] messageId, @Nullable byte[] threadId)
   {
     return HashProvider.getCompositeHashOf(LegacyId.MAESTRO_MESSAGE_ID, tenantId, typeName,
-        fromPod == null ? 0 : fromPod,
         messageId, threadId == null ? new byte[0] : threadId);
   }
 
   /**
    * Create a 2.0 Hash (ID) for an offline notification event.
-   * 
+   *
    * @param tenantId    The tenant ID of the pod where this event originated.
    * This event means that a user was offline when a message was delivered and an email
    * was sent to them.
-   * 
+   *
    * @param toUserId          The id of the offline user.
    * @param messageId         The message ID.
    * @return            The 2.0 object ID for the mirror of the given ID.
@@ -155,7 +153,7 @@ public class LegacyIdFactory
 
   /**
    * Create a 2.0 Hash (ID) for a DeleteEvent.
-   * 
+   *
    * @param tenantId          The tenant ID of the pod where this event originated.
    * @param requesterId       The id of the requesting user.
    * @param deleteMessageId   The message ID of the deleted message.
@@ -170,7 +168,7 @@ public class LegacyIdFactory
 
   /**
    * Create a 2.0 Hash (ID) for a DownloadAttachmentEvent.
-   * 
+   *
    * @param tenantId            The tenant ID of the pod where this event originated.
    * @param downloadedByUserId  The user ID of the user downloading the attachment.
    * @param messageId           The message ID of the attachment.
@@ -184,7 +182,7 @@ public class LegacyIdFactory
 
   /**
    * Create a 2.0 Hash (ID) for a LikeEvent.
-   * 
+   *
    * @param tenantId            The tenant ID of the pod where this event originated.
    * @param liker               The user ID of the user sending the like.
    * @param likedMessageId      The message ID of liked message.
